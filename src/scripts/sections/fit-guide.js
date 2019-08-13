bc409f8eb33b02eec3ab577636669f5e7cf0404e
phpdev <rahulm@shivlab.com>
2019-08-13 18:15:52 +0530

    Fit guide BJ changes developer

--- src/scripts/sections/fit-guide.js
+++ src/scripts/sections/fit-guide.js
@@ -4,20 +4,69 @@ theme.FitGuide = (function() {
             var collectionhandle = $(".collection-fit-guid-click:first").attr('data-collection-handle');
             var parent = $(".collection-fit-guid-click:first").parents('.fit-guid-collection-div');
             var is_first_time = 1;
+            var slider_value = $('[data-id="fitsliderobject"]').attr('data-slider-value');
 
-            load_products(collectionhandle,parent,is_first_time);
+            load_products(collectionhandle,parent,slider_value,is_first_time);
         });
         $(document).on('click','.collection-fit-guid-click',function(){
             var collectionhandle = $(this).attr('data-collection-handle');
             var parent = $(this).parents('.fit-guid-collection-div');
-            load_products(collectionhandle,parent);
+            var slider_value = $('[data-id="fitsliderobject"]').attr('data-slider-value');
+            load_products(collectionhandle,parent,slider_value);
         });
     }
-    function load_products(collectionhandle,parent,is_first_time = 0) {
+    function load_products(collectionhandle,parent,slider_value,is_first_time = 0) {
         var main_class_select_product = parent;
         var title = main_class_select_product.find('.name').html();
         var description = main_class_select_product.find('.fit-guid-description').html();
         var slider_html = "<div class='skinny-part'><h2>"+ title +"</h2><p>" + description + "</p><div class='skinny-slider'>";
+
+        var image_slider_array = slider_value.split('::');
+        var top_image_slider = '';
+        $.each(image_slider_array, function (key, value) {
+            var each_sliderimage = value.split('|');
+            if ( $.trim(each_sliderimage[0]) == collectionhandle){
+                top_image_slider += '<div class="banner-only-images" data-section-id="fit-top-'+ collectionhandle +'">\n' +
+                    '                <img class="desktop_image" src="'+ $.trim(each_sliderimage[1]) +'" alt="desktop_banner">\n' +
+                    '                <img class="desktop_image" src="'+ $.trim(each_sliderimage[2]) +'" alt="mobile_banner">\n' +
+                    '            </div>';
+            }
+        });
+        //$(".fit-image-slider").unslick();
+        $(".fit-image-slider").html(top_image_slider);
+        $('.fit-image-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: false,
+            autoplaySpeed:false,
+            centerPadding: '3px',
+            responsive: [
+                {
+                    breakpoint:991,
+                    settings: {
+                        centerMode: true,
+                        slidesToShow:3
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        centerMode: true,
+                        dots: true,
+                        slidesToShow:2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        centerMode: true,
+                        dots: true,
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+
         $.ajax({
             url: shop_url + "/collections/" + collectionhandle + "/products.json?limit=20",
             type: 'GET'
@@ -103,3 +152,44 @@ theme.FitGuide = (function() {
     FitGuide.prototype = _.assignIn({}, FitGuide.prototype, {});
     return FitGuide;
 })();
+/*
+
+$(document).ready(function() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 768) {
+
+        setTimeout(function() {
+            mob_instagram_slick();
+        }, 5000);
+    }
+});
+
+$(window).on('resize orientationchange', function() {
+    var windowWidth = $(window).width();
+    if (windowWidth < 768) {
+
+        setTimeout(function() {
+            mob_instagram_slick();
+        }, 2000);
+    }
+});
+
+function mob_instagram_slick() {
+    $('.instafeed-mob-slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2, // default desktop values
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        responsive: [{
+            breakpoint: 480, // mobile breakpoint
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+}
+ */
